aed214a42329473f0e47d97fdec1f7fd7a84d29b
Ramkumar Gudivada <rgudivada@damaninc.com>
2026-01-26 17:38:39 -0600

    Fix router path for Vercel support

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/Aruna_website/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' && !process.env.VERCEL ? '/Aruna_website/' : '/',
   plugins: [react()],
-})
+}))
